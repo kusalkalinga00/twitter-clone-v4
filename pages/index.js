524959a9ff0fe4d2b33963be1ae651d64fe80daf
unknown <kusalkalingainfo@gmail.com>
2023-01-02 18:49:15 +0530

    UI complete

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
 import Widgets from "../components/Widgets";
 
-export default function Home({ newsResults }) {
+export default function Home({ newsResults, randomUsersResults }) {
   return (
     <>
       <Head>
@@ -19,7 +19,10 @@ export default function Home({ newsResults }) {
         {/**feed */}
         <Feed />
         {/**widgets */}
-        <Widgets newsResults={newsResults.articles} />
+        <Widgets
+          newsResults={newsResults.articles}
+          randomUsersResults={randomUsersResults.results}
+        />
         {/** Model */}
       </main>
     </>
@@ -32,9 +35,14 @@ export async function getServerSideProps() {
   const newsResults = await fetch(
     "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
   ).then((res) => res.json());
+
+  const randomUsersResults = await fetch(
+    "https://randomuser.me/api/?results=30&inc=name,login,picture"
+  ).then((res) => res.json());
   return {
     props: {
       newsResults,
+      randomUsersResults,
     },
   };
 }
