9ebc7d774a769e41bd643f4fab3d2516aefa44b5
unknown <kusalkalingainfo@gmail.com>
2023-01-02 22:42:39 +0530

    complete sign in page

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-v4-c2efc.firebaseapp.com",
   projectId: "twitter-v4-c2efc",
   storageBucket: "twitter-v4-c2efc.appspot.com",
