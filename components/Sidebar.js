521fcefd5fdd8e9685d535b83688ef281545d087
unknown <kusalkalingainfo@gmail.com>
2023-01-03 15:26:42 +0530

    session added and sign and out functionality

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -11,8 +11,11 @@ import {
   InboxIcon,
   UserIcon,
 } from "@heroicons/react/outline";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Sidebar() {
+  const { data: session } = useSession();
+
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/**twitter logo */}
@@ -29,31 +32,48 @@ export default function Sidebar() {
       <div className="mt-4 mb-2.5 xl:items-start">
         <SidebarMenuItem text="Home" Icon={HomeIcon} active />
         <SidebarMenuItem text="Explore" Icon={HashtagIcon} />
-        <SidebarMenuItem text="Notification" Icon={BellIcon} />
-        <SidebarMenuItem text="Messages" Icon={InboxIcon} />
-        <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
-        <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
-        <SidebarMenuItem text="Profile" Icon={UserIcon} />
-        <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+
+        {session && (
+          <>
+            <SidebarMenuItem text="Notification" Icon={BellIcon} />
+            <SidebarMenuItem text="Messages" Icon={InboxIcon} />
+            <SidebarMenuItem text="Bookmarks" Icon={BookmarkIcon} />
+            <SidebarMenuItem text="Lists" Icon={ClipboardIcon} />
+            <SidebarMenuItem text="Profile" Icon={UserIcon} />
+            <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon} />
+          </>
+        )}
       </div>
 
-      {/**button */}
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Tweet
-      </button>
-      {/**profile */}
+      {session ? (
+        <>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Tweet
+          </button>
 
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          src="https://pbs.twimg.com/profile_images/1439153058487967748/sl9ZO4D2_400x400.jpg"
-          className="h-10 w-10 rounded-full xl:mr-2"
-        />
-        <div className="leading-5  hidden xl:inline">
-          <h4 className="font-bold">Kusal Kalinga</h4>
-          <p className="text-gray-500">@kusal_kalinga</p>
-        </div>
-        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline" />
-      </div>
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              src={session.user.image}
+              className="h-10 w-10 rounded-full xl:mr-2"
+            />
+            <div className="leading-5  hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <DotsHorizontalIcon
+              className="h-5 xl:ml-8 hidden xl:inline"
+              onClick={signOut}
+            />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          sign In
+        </button>
+      )}
     </div>
   );
 }
