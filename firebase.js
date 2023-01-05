e192173269981cc56a1723325879561c9196da2c
unknown <kusalkalingainfo@gmail.com>
2023-01-05 22:51:56 +0530

    final version 1.0

--- firebase.js
+++ firebase.js
@@ -11,8 +11,8 @@ const firebaseConfig = {
   authDomain: "twitter-v4-c2efc.firebaseapp.com",
   projectId: "twitter-v4-c2efc",
   storageBucket: "twitter-v4-c2efc.appspot.com",
-  messagingSenderId: "4865060123",
-  appId: "1:4865060123:web:1af824c2f307659cfaf7be",
+  messagingSenderId: process.env.MESSANGING_SENDER_ID,
+  appId: process.env.APP_ID,
 };
 
 // Initialize Firebase
